e857b1ce81ba4583add6894eabb452cee8abbe31
arcardinal <andrewrcardinal@gmail.com>
2013-11-11 20:58:58 -0600

    Update for geeb

--- slim.mk
+++ slim.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit device configuration
-$(call inherit-product, device/lge/mako/full_mako.mk)
+$(call inherit-product, device/lge/geeb/full_geeb.mk)
 
 # Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
@@ -14,22 +14,19 @@ $(call inherit-product, vendor/slim/config/gsm.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
-# Inherit device settings
-$(call inherit-product, vendor/slim/config/common_nexus.mk)
-
 # Inherit led flash settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := mako
+PRODUCT_RELEASE_NAME := geeb
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := mako
-PRODUCT_NAME := slim_mako
-PRODUCT_BRAND := Google
-PRODUCT_MODEL := Nexus 4
+PRODUCT_DEVICE := geeb
+PRODUCT_NAME := slim_geeb
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG Optimus G
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occam/mako:4.3/JLS36C/573038:user/release-keys" PRIVATE_BUILD_DESC="occam-user 4.3 JLS36C 573038 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occam/geeb:4.3/JLS36C/573038:user/release-keys" PRIVATE_BUILD_DESC="occam-user 4.3 JLS36C 573038 release-keys"
 
